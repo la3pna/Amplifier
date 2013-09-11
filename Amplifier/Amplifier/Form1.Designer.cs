77ec5c3bd61b50b37c64a92c127695a431357f04
la3pna <la3pna@gmail.com>
2013-09-11 16:57:12 +0200

    tab2

--- Amplifier/Amplifier/Form1.Designer.cs
+++ Amplifier/Amplifier/Form1.Designer.cs
@@ -315,7 +315,7 @@
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(878, 527);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "FET bias";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // label7
